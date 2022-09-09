34447059098b0fbea8f2a6e4c2bfe64764942932
HeimdallILVU <113159961+HeimdallILVU@users.noreply.github.com>
2022-09-09 09:24:14 +0200

    Create Projet_ENSEA.ino

---
+++ Capteur infra/Projet_ENSEA/Projet_ENSEA.ino
@@ -0,0 +1,48 @@
+int pin = 5; 
+
+int oldValue = 0; 
+
+int newValue = 0; 
+
+int dValue = 0; 
+
+int threshold = 512; 
+
+int state = 0; 
+
+
+void setup() { 
+
+  Serial.begin(9600); 
+
+  pinMode(pin, OUTPUT); 
+
+} 
+
+  
+
+  
+
+void loop() { 
+
+  newValue = analogRead(A0); 
+
+  Serial.println(newValue); 
+
+  if (newValue >= threshold) { 
+
+    state = 1; 
+
+    digitalWrite(pin, HIGH); 
+
+    } 
+
+  else { 
+
+    state = 0; 
+
+    digitalWrite(pin, LOW); 
+
+    } 
+
+} 
